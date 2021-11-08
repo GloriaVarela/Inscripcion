55be52c7fbc790778514106f4b3614961df6b306
GloriaVarela <gloria_c16@hotmail.com>
2021-11-07 19:40:29 -0500

    operacion get funcional
    Signed-off-by: GloriaVarela <gloria_c16@hotmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const route = express.Router();
+const router = express.Router();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -11,7 +11,11 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://GloriaVarela:jaco201512@clusterc4gloria.ol1h5.mongodb.net/ofertalaboraldb?retryWrites=true&w=majority");
 
 //Operaciones CRUD
-app.use(route);
+router.get('/',(req, res) => {
+    res.send("Este es el inicio de mi primera API");
+});
+
+app.use(router);
 app.listen(3000, ()=> {
     console.log("Servidor corriendo en el puerto 3000")
 });
